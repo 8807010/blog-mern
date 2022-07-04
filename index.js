acc6e03819557c37889eb55f7e72c1e5ab5b54ba
Artiom_Sokolov <sokolova-net@mail.ru>
2022-07-04 21:58:43 +0300

    connect the jsonwebtoken library to connect authentication

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello word');
+});
+
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
