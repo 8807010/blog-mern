a33e8812de0722599384cc02b69591b2cbf4c87b
Artiom_Sokolov <sokolova-net@mail.ru>
2022-07-05 21:02:07 +0300

    create UserModel

--- index.js
+++ index.js
@@ -11,25 +11,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello word');
-});
-
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign({
-    email: req.body.email,
-    fullName: 'Вася Пупкин',
-  },
-    'secret123',
-  );
-
-
-  res.json({
-    success: true,
-    token,
-  });
+app.post('/auth/register', (req, res) => {
+  
 });
 
 app.listen(4444, (err) => {
